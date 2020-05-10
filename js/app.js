266a136cf2c2b4e57c6171b47c6ed9cbe89a3a15
Luca Herrero <lucaherreromarcos@gmail.com>
2020-05-10 08:06:50 +0200

    First Start

---
+++ js/app.js
@@ -0,0 +1,11 @@
+var app = new Framework7({
+    root: '#app',
+    name: 'PlaySync',
+    id: 'com.lhm.playsync',
+    routes: [
+      {
+        path: '/about/',
+        url: 'about.html',
+      },
+    ],
+  });
